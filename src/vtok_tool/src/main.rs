ea675c5f3c5316af0c3ceb5819176cb6b3472500
Dan Horobeanu <dhr@amazon.com>
2020-07-10 14:49:12 +0000

    vtok_rpc: proto and error cleanup
    - Moved all protocol (i.e. unix vs vsock) logic to a new `proto` mod;
    - Rustified error chaning;
    - Removed `fn main()` reliance on the Termination trait (i.e. by
      returning a Result<(), std::io::Error>), since Termination is not yet
      stable; reverted to the old ways of double `main()`.
    
    Signed-off-by: Dan Horobeanu <dhr@amazon.com>
    Reviewed-by: Alexandru Ciobotaru <alcioa@amazon.com>
    Reviewed-by: Bogdan Vasile <bve@amazon.com>
    Reviewed-by: Cristian Done <cdone@amazon.com>

--- src/vtok_tool/src/main.rs
+++ src/vtok_tool/src/main.rs
@@ -1,7 +1,31 @@
 extern crate vtok_rpc;
 
+use std::fmt;
 use std::io::Write;
 use vtok_rpc::{ProvisionProto, Stream};
+
+enum Error {
+    ProtoError(vtok_rpc::ProtoError),
+    IoError(std::io::Error),
+}
+
+impl From<Error> for i32 {
+    fn from(_other: Error) -> i32 {
+        // NOTE: we could discriminate between errors here to provide a more specific
+        // exit code.
+        1
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // TODO: pretty-format error messages
+        match self {
+            Self::ProtoError(e) => write!(f, "{:?}", e),
+            Self::IoError(e) => write!(f, "{:?}", e),
+        }
+    }
+}
 
 fn print_usage() {
     let usage = r#"Nitro vToken Tool
@@ -14,7 +38,7 @@ Usage:
 /// Parameters:
 /// AF_VSOCK: <nitro-vtoken> "vsock" "16" "10000"
 /// AF_UNIX:  <nitro-vtoken> "unix" "some/path"
-fn main() -> Result<(), std::io::Error> {
+fn main_main() -> Result<(), Error> {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() < 2 {
@@ -25,13 +49,23 @@ fn main() -> Result<(), std::io::Error> {
     // TODO: Lots of logic to add
     //
     // Spawn a client and send some bytes
-    let proto = ProvisionProto::from_args(&args[1..])?;
-    let mut stream = Stream::new(&proto)?;
+    let proto = ProvisionProto::from_args(&args[1..]).map_err(Error::ProtoError)?;
+    let mut stream = Stream::new(&proto).map_err(Error::ProtoError)?;
 
     let data = "Testing the client".as_bytes();
-    stream.write_all(&data)?;
+    stream.write_all(&data).map_err(Error::IoError)?;
 
     println!("Done");
 
     Ok(())
+}
+
+fn main() {
+    match main_main() {
+        Ok(()) => std::process::exit(0),
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(i32::from(e))
+        }
+    }
 }
