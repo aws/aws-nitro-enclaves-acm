9c9a94184b0debb2b51cbd81c2d0472d410d055c
Alexandru Ciobotaru <alcioa@amazon.com>
2020-08-03 15:29:35 +0300

    Add Copyright Headers
    Signed-off-by: Alexandru Ciobotaru <alcioa@amazon.com>
    Reviewed-by: Bogdan Vasile <bve@amazon.com>
    Reviewed-by: Dan Horobeanu <dhr@amazon.com>

--- src/vtok_p11/src/crypto/decrypt.rs
+++ src/vtok_p11/src/crypto/decrypt.rs
@@ -1,3 +1,6 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::backend::Mechanism;
 
 use super::config_evp_pkey_ctx;
--- src/vtok_p11/src/crypto/ffi.rs
+++ src/vtok_p11/src/crypto/ffi.rs
@@ -1,3 +1,6 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 
