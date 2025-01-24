e254c37f56f516fd9cb444d4dcf89fbb0e95f5b6
Belhoussine <belhou@amazon.de>
2025-01-24 14:02:26 +0000

    cdk: Fix httpd bug on AL2023 - resolve missing (undocumented) httpd-acm.conf directives and edit openssl.cnf

--- streamline/src/assets/user-data-scripts/AL2023/apache-conf.sh
+++ streamline/src/assets/user-data-scripts/AL2023/apache-conf.sh
@@ -9,8 +9,11 @@ sudo sed -i 's!certificate_arn: ""!certificate_arn: "CERTIFICATE_ARN_PLACEHOLDER
 
 # Generate the /etc/httpd/conf.d/httpd-acm.conf file
 sudo mv /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/ssl.conf-bkp
-printf "<VirtualHost *:443>\nServerName DOMAIN_NAME_PLACEHOLDER\nSSLEngine on\nSSLProtocol -all +TLSv1.2\nSSLCertificateKeyFile ""\nSSLCertificateFile ""\n</VirtualHost>\n" | sudo tee /etc/httpd/conf.d/httpd-acm.conf
+printf "Listen 443 https\nSSLPassPhraseDialog exec:/usr/libexec/httpd-ssl-pass-dialog\nSSLCryptoDevice pkcs11\n<VirtualHost *:443>\nServerName DOMAIN_NAME_PLACEHOLDER\nSSLEngine on\nSSLProtocol -all +TLSv1.2\nSSLCertificateKeyFile /etc/pki/tls/private/localhost.key\nSSLCertificateFile /etc/pki/tls/certs/localhost.crt\n</VirtualHost>\n" | sudo tee /etc/httpd/conf.d/httpd-acm.conf
 
-# Start the ACM for Nitro Enclaves service
+# Edit the OpenSSL configuration /etc/pki/tls/openssl.cnf
+sudo sed -i '/ssl_conf = ssl_module/a\engines = engine_section\n\n[engine_section]\npkcs11 = pkcs11_section\n\n[ pkcs11_section ]\nengine_id = pkcs11\ninit = 1' /etc/pki/tls/openssl.cnf
+
+# # Start the ACM for Nitro Enclaves service
 sudo systemctl start nitro-enclaves-acm.service
 sudo systemctl enable nitro-enclaves-acm
